85b1146c211358c45858cc64b511248ecda2e739
Fabrice <fabrice.cantin@harmonie-mutuelle.fr>
2021-02-10 19:38:57 +0100

    initial commits

--- MistLegacy/urls.py
+++ MistLegacy/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django_distill import distill_path
 from front.views import test
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def get_index():
@@ -26,4 +28,4 @@ def get_index():
 urlpatterns = [
     path('admin/', admin.site.urls),
     distill_path('', test, name='index', distill_func=get_index, distill_file='index.html'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
