971714b5bc74ae8d9ed01d3bb0f2baa23b49acf8
Fabrice <fabrice.cantin@harmonie-mutuelle.fr>
2021-02-10 19:15:03 +0100

    initial commits

--- MistLegacy/settings.py
+++ MistLegacy/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'C:/distill'
+STATIC_ROOT = '/home/Nacros/mldb/static'
 
 MODELTRANSLATION_FALLBACK_LANGUAGES = ('en', 'fr')
 gettext = lambda s: s
