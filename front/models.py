eee6902a0b935e63e6e5c17e1d1449202fe5c306
Fabrice <fabrice.cantin@harmonie-mutuelle.fr>
2021-05-22 11:26:17 +0200

    Ajout des ingrédients

--- front/models.py
+++ front/models.py
@@ -389,6 +389,9 @@ class MaterialType(models.Model):
 class IngredientType(models.Model):
     name = models.CharField(max_length=64, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ["name"]
 
